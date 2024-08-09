a23f31b7a01108f1b6081b02ca2e2889f88a3261
Trisemya <trisemya@gmail.com>
2024-08-09 19:07:23 +0300

    init

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 const pool = mysql.createPool({
@@ -10,18 +12,48 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME
 });
 
-app.get('/users', async (req, res) => {
-  try {
-    const [rows, fields] = await pool.query('SELECT * FROM users');
-    console.log(rows); // Выводим результаты в консоль
-    res.json(rows); // Отправляем результаты в ответе
-  } catch (error) {
-    console.error(error); // Логируем ошибку
-    res.status(500).send('Ошибка при получении данных'); // Отправляем сообщение об ошибке
-  }
-});
+app.use(cors())
+
+const jsonParser = bodyParser.json()
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
+});
+
+
+
+app.get('/users', async (req, res) => {
+  try {
+    const [rows, fields] = await pool.query('SELECT * FROM users');
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Ошибка при получении данных'); // Отправляем сообщение об ошибке
+  }
+});
+
+app.get('/users/:tg_id', async (req, res) => {
+    try {
+      const [rows, fields] = await pool.query('SELECT * FROM users where tg_id=' + req.params.tg_id);
+      if (!rows[0]) {
+        return res.status(404).send('Нет такого пользователя!');
+      }
+      res.json(rows[0]);
+    } catch (error) {
+      console.error(error); 
+      res.status(500).send('Ошибка при получении данных'); 
+    }
+})
+
+app.post('/users', jsonParser, async (req, res) => {
+  try {
+    const {tg_id, name} = req.body;
+    const [rows, fields] = await pool.query(`insert into users (tg_id, name, villagers) values (${tg_id}, '${name}', 0)`);
+    return res.status(201).send(req.body)
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Ошибка при получении данных');
+  }
 });
